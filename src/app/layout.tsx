4f5a6bbc05981b450006f6380cb6e6c492bd8dda
nazmul910 <islamnasir910@gmail.com>
2026-07-17 23:32:41 +0600

    Add new WebP images for Visual Studio Code and windsurfing graphics
    - Introduced a new WebP image for Visual Studio Code located at public/vscode.webp.
    - Added a new WebP image for windsurfing located at public/windsurf.webp.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,16 @@ export default function RootLayout({
 
       <body className={`${geistSans.className} ${geistMono.className} bg-zinc-950 text-zinc-100 antialiased  `}>
         <div className="min-h-full flex">
-          {children}
+          <Sidebar/>
+          <div className="flex-1 md:ml-64 ml-10px">
+            <main className="min-h-screen">
+              {children}
+            </main>
+            <Footer/>
+          </div>
+
         </div>
+        
 
       </body>
     </html>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import React,{useState,useEffect} from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import {sidebarStyles as s } from "../../public/dummyStyles";
+
 export default function Sidebar() {
   return (
     <main>
-        
+
     </main>
   );
 }

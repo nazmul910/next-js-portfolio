ca2cbff834f1d5ec03a19e9b6cfea4f863066469
nazmul910 <islamnasir910@gmail.com>
2026-07-22 23:25:12 +0600

    feat: rename Projects component to ProjectsPage for consistency and clarity

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,11 +3,28 @@
 import { useRouter } from "next/router";
 import {projectStyles as s} from "../../../public/dummyStyles"
 
+import {projects,Project} from "@/lib/projects-data"
 
-export default function Projects() {
+
+export default function ProjectsPage() {
   return (
     <>
-      
+      <div className={s.pageContainer}>
+        <div className={s.innerContainer}>
+          <div className={s.header}>
+            <h1 className={s.pageTitle}>
+              Projects
+            </h1>
+            <p className={s.pageSubtitle}>
+              Playground - Small MVP to Production Apps
+
+            </p>
+
+          </div>
+
+        </div>
+
+      </div>
     </>
   );
 }
